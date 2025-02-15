dfbb5e52d665562e1173e0f7df7e98f63684144e
bhupendra-64 <bhupendras.ug24.ce@nitp.ac.in>
2025-02-15 12:12:16 +0530

    files add

---
+++ ecommerce/src/App.jsx
@@ -0,0 +1,32 @@
+import React from "react";
+const App = ()=>{
+  return(
+    <>
+    <div className="main">
+      <div className="logo">
+        <p>
+          Fassion <span> Hub </span>
+        </p>
+      </div>
+      <div className="searchBar">
+        <input type="text" placeholder="Search fassionHub.com"></input>
+      </div>
+      <div className="about">
+        <a id="abt" href="#" target="_main">About</a>
+
+      </div>
+      <div className="contact">
+        <a id="cnt" href="#" target="_main">contactUs</a>
+
+      </div>
+      <div className="myAccount">
+        
+      </div>
+
+
+    </div>
+    </>
+  )
+
+}
+export default App;
